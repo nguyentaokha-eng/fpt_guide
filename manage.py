1293d0c3635bfe8f9e353a4f75ae1c0a227c6165
nguyen <nguyentaokha@gmail.com>
2026-03-18 21:37:58 +0700

    update

--- manage.py
+++ manage.py
@@ -1,22 +1,42 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
+#Q1
+class StudentEnrollment:
+    def __init__(self, student_id, student_name, program, department, start_year):
+        self.student_id = str(student_id)
+        self.student_name = str(student_name)
+        self.program = str(program)
+        self.department = str(department)
+        self.__start_year = int(start_year)
+    def get_start_year(self):
+        print (f'Start year: {self.__start_year}')
+    def set_start_year(self, year):
+        if year > 0:
+            self.__start_year = year
+            return self.__start_year
+        else:
+            print('invalid year')
+    def __str__(self):
+        return f'Student ID: {self.student_id}, Student Name: {self.student_name}, Program: {self.program}, Department: {self.department}, Start Year: {self.__start_year}'     
+        
+student = StudentEnrollment('DE201022', 'A', 'Eng', 'P213', 2024)
+student.set_start_year(2025)
+print(student.set_start_year(2025))
+print(student)
 
-
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fpt_guide.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
+#Q2
+filename = 'student_enrollment_data.txt'
+def load_enrollment_data(filename):
+    with open(filename) as file:
+        lst = []
+        next(file)
+        for line in file:
+            info = line.strip().split(' ')
+            student_id = info[0]
+            student_name = info[1]
+            program = info[2]
+            department = info[3]
+            start_year = int(info[4])
+            obj = StudentEnrollment(student_id, student_name, program, department, start_year)
+            lst.append(obj)
+    return lst
+enrollment_data = load_enrollment_data(filename)
+print(enrollment_data)
